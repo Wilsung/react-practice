8bc01aec9831f8c03030fcaaf277f7c93cc00f6d
Wilson <wou123@yahoo.com>
2024-01-15 12:09:12 -0800

    nothing

--- starting-project/src/components/Log.jsx
+++ starting-project/src/components/Log.jsx
@@ -1,11 +1,12 @@
 export default function Log ( {turns} ){
+    
     for (const turn of turns){
         const { square, player } = turn;
         const { row, col } = square;
 
         gameBoard[row][col] = player;
     }
-    
+
     return (
         <div>hi</div>
     );
