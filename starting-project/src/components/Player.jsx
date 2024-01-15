c39ebed77c8b3e3e4e59cef33fe70797f3d01fab
Wilson <wou123@yahoo.com>
2024-01-14 21:26:10 -0800

    added players

---
+++ starting-project/src/components/Player.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+export default function Player ( {initialName, symbol} ) {
+    const [ playerName, setPlayerName ] = useState(initialName);
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function handleEditClick (){
+        // We should not be coding this way, schedules for React
+        // to update state at a later time.
+        // setIsEditing(!isEditing);
+
+        //do this. this gets latest state value.
+        setIsEditing((editing) => !(editing));
+    }
+
+    //even is used from onChange (built-in process) -recording user keystrokes.
+    function handleNameChange (event){
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
+
+    
+
+    if (isEditing){
+        //onChange listens to every keystroke entered by the user. 
+        //this is a two way binding - input leaves and enters.
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleNameChange}/>
+    }
+
+    return (
+        <li>
+        <span className="player">
+            {editablePlayerName}
+            <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    );
+}
